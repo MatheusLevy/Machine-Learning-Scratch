84278edc19991269b1bdbb9cfdf150a00dd2185d
Matheus <matheus.levy@discente.ufma.br>
2024-02-29 14:34:48 -0300

    [ADD] Data headers

---
+++ Machine Learning/Machine Learning/Machine Learning.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    uint8_t meuUInt8 = -22;
+    std::cout << "Valor de meuUInt8: " << static_cast<int>(meuUInt8) << std::endl;
+
+}
---
+++ Machine Learning/Machine Learning/data_handler.hpp
@@ -0,0 +1,41 @@
+#ifndef __DATA_HANDLER_H
+#define __DATA_HANDLER_H
+
+#include <fstream>
+#include "stdint.h"
+#include "data.hpp"
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_set>
+
+class Data_Handler {
+	std::vector<Data *> *data; // all data not splited
+	std::vector<Data*> *training_data;
+	std::vector<Data*> *validation_data;
+	std::vector<Data*> *test_data;
+	
+	int num_classes;
+	int feature_vector_size;
+	std::map<uint8_t, int> class_map;
+
+	const double TRAIN_SET_PERCENTE = 0.75;
+	const double TEST_SET_PERCENT = 0.20;
+	const double VALIDATION_SET_PERCENT = 0.05;
+
+	public:
+		Data_Handler();
+		~Data_Handler();
+
+		void read_feature_vector(std::string path);
+		void read_feature_labels(std::string path);
+		void split_data();
+		void count_classes();
+
+		uint32_t convert_to_little_endian(const unsigned char *bytes);
+
+		std::vector<Data*> get_training_data();
+		std::vector<Data*> get_validation_data();
+		std::vector<Data*> get_testing_data();
+};
+#endif
